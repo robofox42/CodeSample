3b79af7849eee404eed7b63ae8b61ad81d7f1f42
Laren <larenmortensen@gmail.com>
2021-04-23 02:14:05 -0600

    Add Spring Boot skeleton

---
+++ src/main/java/com/example/restApp/StudentClassesApplication.java
@@ -0,0 +1,13 @@
+package com.example.restApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentClassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restApp/StudentClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
